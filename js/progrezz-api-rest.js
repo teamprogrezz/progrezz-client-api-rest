47a988d54e2c75c29febae7fac1c78a082e96b66
Wikiti <wikiti.doghound@gmail.com>
2015-03-01 18:13:42 +0000

    Mejorando algunos detalles.

--- js/progrezz-api-rest.js
+++ js/progrezz-api-rest.js
@@ -1,50 +1,118 @@
-function GenericUtils() {}
-GenericUtils.cmpVersion = function(a, b) {
-    var i, cmp, len, re = /(\.0)+[^\.]*$/;
-    a = (a + '').replace(re, '').split('.');
-    b = (b + '').replace(re, '').split('.');
-    len = Math.min(a.length, b.length);
-    for( i = 0; i < len; i++ ) {
-        cmp = parseInt(a[i], 10) - parseInt(b[i], 10);
-        if( cmp !== 0 ) {
-            return cmp;
-        }
-    }
-    return a.length - b.length;
+// ---------------------------
+// -  Utilidades genéricas   -
+// ---------------------------
+function GenericUtils() { }
+
+//         Versionado
+// ---------------------------
+GenericUtils.versions = function() { }
+
+// Comparar versiones arbitrarias como strings ("1.1.0", "1.2", "1", "3.0.2.3", etc).
+GenericUtils.versions.cmpVersion = function(a, b) {
+  var i, cmp, len, re = /(\.0)+[^\.]*$/;
+  a = (a + '').replace(re, '').split('.');
+  b = (b + '').replace(re, '').split('.');
+  len = Math.min(a.length, b.length);
+  for( i = 0; i < len; i++ ) {
+      cmp = parseInt(a[i], 10) - parseInt(b[i], 10);
+      if( cmp !== 0 ) {
+          return cmp;
+      }
+  }
+  return a.length - b.length;
 }
 
-GenericUtils.gteVersion = function(a, b) {
-    return GenericUtils.cmpVersion(a, b) >= 0;
+// Comparar versiones (mayor que). Véase GenericUtils.cmpVersion
+GenericUtils.versions.gteVersion = function(a, b) {
+    return GenericUtils.versions.cmpVersion(a, b) >= 0;
 }
 
-GenericUtils.ltVersion = function(a, b) {
-    return GenericUtils.cmpVersion(a, b) < 0;
+// Comparar versiones (menor que). Véase GenericUtils.cmpVersion
+GenericUtils.versions.ltVersion = function(a, b) {
+    return GenericUtils.versions.cmpVersion(a, b) < 0;
 }
 
-GenericUtils.escapeHTML = function( string )
-{
+//            HTML
+// ---------------------------
+GenericUtils.html = function() { }
+
+// Escapar entidades HTML de una cadena para evitar convertirla en elementos del DOM al volcarla sobre un XHTML.
+GenericUtils.html.escape = function( string ) {
     var pre = document.createElement('pre');
     var text = document.createTextNode( string );
     pre.appendChild(text);
     return pre.innerHTML;
 }
 
+//             XML
+// ---------------------------
+GenericUtils.xml = function() { }
+
+// Función para parsear ficheros XML teniendo en cuenta compatibilidad con diversos exploradores.
+GenericUtils.xml.parse = function ( xml_str ) {
+  var xmlDoc = null
+  var parser = null
+  
+  // Mayoría de exploradores
+  if (window.DOMParser) {
+    parser=new DOMParser();
+    xmlDoc=parser.parseFromString(txt,"text/xml");
+  }
+  // Internet explorar (sin compatibilidad)
+  else {
+    xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+    xmlDoc.async=false;
+    xmlDoc.loadXML(txt);
+  }
+  
+  return xmlDoc
+}
+
+// Función para stringificar objetos XML.
+GenericUtils.xml.stringify = function ( xml ) {
+  if (window.ActiveXObject) 
+    return xml.xml;
+  else
+    return new XMLSerializer().serializeToString(xml);
+}
+
+// ---------------------------
+// -      Utilidad REST      -
+// ---------------------------
 function RESTResquest() {
   this.onComplete = null
   this.onFail = null
   this.onEnd = null
 }
 
-RESTResquest.DEFAULT_REQUEST      = { type: "test" }
-RESTResquest.DEFAULT_REQUEST_URL  = "http://localhost:4567/dev/api/rest"
-RESTResquest.DEFAULT_REQUEST_TYPE = "GET"
-RESTResquest.DEFAULT_TYPE         = "json"
+RESTResquest.DEFAULT = function() { }
+
+// Constantes
+RESTResquest.DEFAULT.REQUEST        = { type: "echo", data: { name: "world" } }
+RESTResquest.DEFAULT.REQUEST_URL    = "http://localhost:4567/dev/api/rest"
+RESTResquest.DEFAULT.REQUEST_TYPE   = "GET"
+RESTResquest.DEFAULT.TIMESTAMP_FUNC = function() { return new Date().getTime() }
+RESTResquest.DEFAULT.TIMEOUT        = 0
+RESTResquest.DEFAULT.TYPE           = "json"
+
+// Plantilla por defecto
 RESTResquest.getTemplateRequest = function() {  
-  return { metadata: { request_url: RESTResquest.DEFAULT_REQUEST_URL, timestamp: new Date().getTime(), type: RESTResquest.DEFAULT_TYPE, request_type: RESTResquest.DEFAULT_REQUEST_TYPE/* ... */ }, request: RESTResquest.DEFAULT_REQUEST }
+  return {
+    metadata: {
+      request_url:  RESTResquest.DEFAULT.REQUEST_URL,      // URL de petición
+      timestamp:    RESTResquest.DEFAULT.TIMESTAMP_FUNC(), // Marca de fecha actual, en ms
+      timeout:      RESTResquest.DEFAULT.TIMEOUT,          // Timeout de la petición
+      type:         RESTResquest.DEFAULT.TYPE,             // Tipo de archivo solicitado (json, xml, plain-text, ...)
+      request_type: RESTResquest.DEFAULT.REQUEST_TYPE      // Tipo de petición http (get, post, ...)
+      /* ... */
+    },
+    request: RESTResquest.DEFAULT.REQUEST             // Petición por defecto
+  }
 }
 
-// If jquery 1.5.0 or above is found, it will use getJSON method.
-if( typeof $ !== 'undefined' && GenericUtils.gteVersion($.fn.jquery, "1.5.0")) {
+// Si se encuentra jQuery 1.5.0 (o versiones superiores), se usarán peticiones ajax de jQuery.
+if( typeof $ !== 'undefined' && GenericUtils.versions.gteVersion($.fn.jquery, "1.5.0")) {
+  // Habilitar CORS
   $.support.cors = true;
 
   //   Request method. A json file must be providen. See documentation
@@ -61,9 +129,11 @@ if( typeof $ !== 'undefined' && GenericUtils.gteVersion($.fn.jquery, "1.5.0")) {
         if(self.onError != null) self.onError(xhr);
         if(self.onEnd   != null) self.onEnd();
       },
-      success:  function(ajaxResponse) {        
-        if (typeof ajaxResponse == "string")
+      success:  function(ajaxResponse) {
+        if (typeof ajaxResponse == "string") {
           ajaxResponse = $.parseJSON(ajaxResponse);
+        }
+        ajaxResponse.request = json // Añadir petición a la respuesta.
         
         if(self.onComplete != null) self.onComplete(ajaxResponse);
         if(self.onEnd      != null) self.onEnd();
